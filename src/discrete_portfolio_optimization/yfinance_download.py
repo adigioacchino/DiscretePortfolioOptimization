8137498bb1a00cc43773584d585928ed5ff23311
adagio <andrea.dgioacchino@gmail.com>
2024-09-07 16:32:27 +0200

    :memo: add some docstrings

--- src/discrete_portfolio_optimization/yfinance_download.py
+++ src/discrete_portfolio_optimization/yfinance_download.py
@@ -5,11 +5,15 @@ from tqdm import tqdm
 import marimo as mo  # to have marimo-compatible progress bars
 
 
-def download_close_price(ticker: str):
+def download_close_price(ticker: str) -> pd.Series:
     """
     Download the close price of a stock from Yahoo Finance.
-    :param ticker: str, the ticker of the stock
-    :return: pd.Series, the close price of the stock
+
+    Args:
+        ticker: str, the ticker (name) of the stock to download
+
+    Returns:
+        pd.Series, the close price of the stock
     """
     data = yf.download(ticker, period="max", progress=False)
     return data["Close"]
@@ -18,8 +22,12 @@ def download_close_price(ticker: str):
 def get_close_price_df(tickers: str) -> pd.DataFrame:
     """
     Download the close price of a list of stocks from Yahoo Finance.
-    :param tickers: str, the tickers of the stocks separated by comma
-    :return: pd.DataFrame, the close price of the stocks
+
+    Args:
+        tickers: str, a string of tickers separated by a comma
+
+    Returns:
+        pd.DataFrame, the close price of the stocks
     """
     all_data = dict()
     symbols = [x.strip() for x in tickers.split(",")]
